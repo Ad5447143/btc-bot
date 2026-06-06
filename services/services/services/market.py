3fd6dc9823cccf2b6b1cf778344b9af610d415de
Ad5447143 <ad5447143@gmail.com>
2026-06-06 15:53:56 +0330

    market.py

--- services/services/services/market.py
+++ services/services/services/market.py
@@ -1,1 +1,9 @@
+import requests
 
+def get_price(symbol):
+    try:
+        url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
+        data = requests.get(url).json()
+        return float(data["price"])
+    except:
+        return None
