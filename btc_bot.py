94c6eec2afac51420f82bbc6fe6549a13dd606c8
Ad5447143 <ad5447143@gmail.com>
2026-06-06 16:18:10 +0330

    btc_bot.py

--- btc_bot.py
+++ btc_bot.py
@@ -7,38 +7,35 @@ from services.scanner import run_scanner
 
 print("BOT STARTED 🚀")
 
-# =========================
+# ======================
 # START COMMAND
-# =========================
+# ======================
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🤖 ربات فعال شد و آماده کار است")
+    await update.message.reply_text("🤖 ربات فعال شد")
 
-# =========================
-# MAIN FUNCTION
-# =========================
+# ======================
+# MAIN
+# ======================
 def main():
 
-    # ساخت اپلیکیشن
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    # هندلر /start
     app.add_handler(CommandHandler("start", start))
 
-    # =========================
-    # اجرای اسکنر در پس‌زمینه
-    # =========================
-    scanner_thread = threading.Thread(
+    # ======================
+    # SCANNER THREAD
+    # ======================
+    t = threading.Thread(
         target=run_scanner,
         args=(app.bot, 369031827),
         daemon=True
     )
-    scanner_thread.start()
+    t.start()
 
     print("Bot is running...")
 
-    # اجرای ربات
     app.run_polling()
 
-# =========================
+# ======================
 if __name__ == "__main__":
     main()
