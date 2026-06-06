3d895082601d80d45f8f727c5898a8f001ea8607
Ad5447143 <ad5447143@gmail.com>
2026-06-06 16:22:52 +0330

    market.py

--- services/services/services/market.py
+++ services/services/services/market.py
@@ -1,9 +1,30 @@
 import requests
 
+# تبدیل کوین به id کوین‌گکو
+COIN_MAP = {
+    "BTCUSDT": "bitcoin",
+    "ETHUSDT": "ethereum",
+    "BNBUSDT": "binancecoin",
+    "SOLUSDT": "solana",
+    "XRPUSDT": "ripple",
+    "DOGEUSDT": "dogecoin",
+    "ADAUSDT": "cardano",
+    "TRXUSDT": "tron"
+}
+
 def get_price(symbol):
+
     try:
-        url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
+        coin_id = COIN_MAP.get(symbol)
+
+        if not coin_id:
+            return None
+
+        url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies=usd"
+
         data = requests.get(url).json()
-        return float(data["price"])
+
+        return data[coin_id]["usd"]
+
     except:
         return None
