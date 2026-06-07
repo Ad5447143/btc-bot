d55c88dc0bb0e52e68fcee7c911c615a78c4874e
Ad5447143 <ad5447143@gmail.com>
2026-06-07 14:43:51 +0330

    market.py

--- services/market.py
+++ services/market.py
@@ -1,1 +1,32 @@
-# market service
+import requests
+
+COIN_MAP = {
+    "BTCUSDT": "bitcoin",
+    "ETHUSDT": "ethereum",
+    "BNBUSDT": "binancecoin",
+    "SOLUSDT": "solana",
+    "XRPUSDT": "ripple",
+    "DOGEUSDT": "dogecoin",
+    "ADAUSDT": "cardano",
+    "TRXUSDT": "tron"
+}
+
+def get_price(symbol):
+    try:
+        coin_id = COIN_MAP.get(symbol)
+
+        if not coin_id:
+            return None
+
+        url = (
+            "https://api.coingecko.com/api/v3/simple/price"
+            f"?ids={coin_id}&vs_currencies=usd"
+        )
+
+        data = requests.get(url, timeout=10).json()
+
+        return data[coin_id]["usd"]
+
+    except Exception as e:
+        print("Market Error:", e)
+        return None
