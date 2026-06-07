4eb743002795405b99224ebaff0f48fe2439cb6c
Ad5447143 <ad5447143@gmail.com>
2026-06-07 14:46:20 +0330

    scanner.py

--- services/scanner.py
+++ services/scanner.py
@@ -1,9 +1,37 @@
-# scanner service
+import time
 
 from config import COINS
+from services.signal_engine import generate_signal
+
+last_signals = {}
 
 def run_scanner():
-    print("Scanner started")
 
-    for coin in COINS.keys():
-        print("Scanning", coin)
+    print("🚀 Scanner Started")
+
+    while True:
+
+        try:
+
+            for coin in COINS.keys():
+
+                signal, score = generate_signal(coin)
+
+                if score >= 4:
+
+                    if last_signals.get(coin) == signal:
+                        continue
+
+                    last_signals[coin] = signal
+
+                    print(
+                        f"🟢 {coin} | Signal Score: {score}"
+                    )
+
+        except Exception as e:
+
+            print(
+                f"Scanner Error: {e}"
+            )
+
+        time.sleep(300)
