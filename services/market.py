57d779a46bc8b2951074933bde234eef5501bfa6
Ad5447143 <ad5447143@gmail.com>
2026-06-07 16:35:35 +0330

    market.py

--- services/market.py
+++ services/market.py
@@ -1,4 +1,6 @@
 import requests
+import pandas as pd
+from ta.momentum import RSIIndicator
 
 
 def get_price(symbol="bitcoin"):
@@ -16,6 +18,45 @@ def get_price(symbol="bitcoin"):
         ).json()
 
         return data[symbol]["usd"]
+
+    except:
+
+        return None
+
+
+def get_rsi(symbol="bitcoin"):
+
+    try:
+
+        url = (
+            "https://api.coingecko.com/api/v3/coins/"
+            f"{symbol}/market_chart?vs_currency=usd&days=30"
+        )
+
+        data = requests.get(
+            url,
+            timeout=10
+        ).json()
+
+        prices = [
+            p[1]
+            for p in data["prices"]
+        ]
+
+        df = pd.DataFrame(
+            prices,
+            columns=["close"]
+        )
+
+        rsi = RSIIndicator(
+            close=df["close"],
+            window=14
+        ).rsi()
+
+        return round(
+            float(rsi.iloc[-1]),
+            2
+        )
 
     except:
 
